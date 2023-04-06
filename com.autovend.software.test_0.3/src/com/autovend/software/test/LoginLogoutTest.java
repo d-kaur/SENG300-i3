c665fdc82ead5ab64eac5f835126e280aa2374fc
GillianSharp <127808902+GillianSharp@users.noreply.github.com>
2023-04-06 17:43:43 -0600

    Update LoginLogoutTest.java

--- com.autovend.software.test_0.3/src/com/autovend/software/test/LoginLogoutTest.java
+++ com.autovend.software.test_0.3/src/com/autovend/software/test/LoginLogoutTest.java
@@ -1,3 +1,6 @@
+//Gillian Sharp 30142574
+//Johnathan 
+
 package com.autovend.software.test;
 
 import com.autovend.software.*;
@@ -7,6 +10,10 @@ import org.junit.Test;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class LoginLogoutTest {
 
@@ -38,4 +45,50 @@ public class LoginLogoutTest {
 		Boolean expected = false;
 		assertEquals(actual, expected);
 	}
+	
+     public class AttendantIOTest {
+
+   	private AttendantIO attendantIO;
+
+   	@BeforeEach
+   	public void setUp() {
+      	  attendantIO = new AttendantIO();
+  	 }
+
+   	@Test
+   	public void testLogoutSuccess() {
+      	  attendantIO.logout();
+      	  Assertions.assertFalse(attendantIO.loggedIn);
+   	}
+	 
+	@Test
+   	public void testLogoutNotLoggedIn() {
+      	  attendantIO.logout();
+          attendantIO.logout(); // Attempt to logout again
+          Assertions.assertFalse(attendantIO.loggedIn);
+        }
+
+   	@Test
+   	public void testLogoutMessage() {
+      	  ConsoleOutputCapturer coc = new ConsoleOutputCapturer();
+          coc.start();
+          attendantIO.logout();
+          coc.stop();
+          String expectedMessage = "Successfully logged out.";
+          String actualMessage = coc.getOutput().trim();
+          Assertions.assertEquals(expectedMessage, actualMessage);
+         }
+	     
+	@Test
+   	public void testLogoutNotLoggedInMessage() {
+      	  ConsoleOutputCapturer coc = new ConsoleOutputCapturer();
+      	  coc.start();
+      	  attendantIO.logout();
+          attendantIO.logout(); // Attempt to logout again
+          coc.stop();
+          String expectedMessage = "You are not currently logged in.";
+          String actualMessage = coc.getOutput().trim();
+          Assertions.assertEquals(expectedMessage, actualMessage);
+       }
+    }
 }
