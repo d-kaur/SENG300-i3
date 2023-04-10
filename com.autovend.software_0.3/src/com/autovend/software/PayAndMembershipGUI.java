bd587b2486aa5b03f13e1e7e2dfefea96718fb70
Deepinder Kaur <56122609+deepinder-10@users.noreply.github.com>
2023-04-10 03:45:01 -0600

    😭

--- com.autovend.software_0.3/src/com/autovend/software/PayAndMembershipGUI.java
+++ com.autovend.software_0.3/src/com/autovend/software/PayAndMembershipGUI.java
@@ -1,29 +1,52 @@
 package com.autovend.software;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class PayAndMembershipGUI extends JFrame implements ActionListener {
+public class PayAndMembershipGUI extends JFrame {
 
-   /* private JButton membershipButton;
-    private JLabel payTypeLabel;
-    private JButton payCardButton;
-    private JButton giftCardButton;
-    private JButton payCashButton;
-    private JButton back;
+    private JButton card;
+    private JButton giftCard;
+    private JButton cash;
+    private JButton enterMembership;
 
-*/
-    private PayWithCard card;
-    private PayWithCash cash;
 
     public PayAndMembershipGUI(){
         JFrame frame = new JFrame("Pay & Membership");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
 
+        JPanel cardPanel = new JPanel();
+        JPanel cashPanel = new JPanel();
+        JPanel giftCardPanel = new JPanel();
+        JPanel membershipPanel = new JPanel();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+        setMinimumSize(new Dimension(600, 400));
 
-    }
-}
+        card = new JButton("Debit/Credit");
+        card.addActionListener(e -> {
+
+        });
+
+        cash = new JButton("Cash");
+        cash.addActionListener(e -> {
+
+        });
+
+        giftCard = new JButton("Gift Card");
+        cash.addActionListener(e -> {
+
+        });
+
+        enterMembership = new JButton("Enter Membership");
+        enterMembership.addActionListener(e -> {
+
+        });
+
+       JPanel content = new JPanel(new GridLayout(3,2));
+       content.add(cardPanel);
+       content.add(giftCardPanel);
+       content.add(cashPanel);
+       content.add(membershipPanel);
+    } //PayAndMembershipGUI end
+} //class end
