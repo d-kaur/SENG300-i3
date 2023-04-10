c190b2e8022ef1f85802edb54e618ac74c2f03d1
Deepinder Kaur <56122609+deepinder-10@users.noreply.github.com>
2023-04-10 00:15:51 -0600

    😭

--- com.autovend.software_0.3/src/com/autovend/software/PayAndMembershipGUI.java
+++ com.autovend.software_0.3/src/com/autovend/software/PayAndMembershipGUI.java
@@ -1,15 +1,29 @@
 package com.autovend.software;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PayAndMembershipGUI extends JFrame {
+public class PayAndMembershipGUI extends JFrame implements ActionListener {
 
-    private JButton membershipButton;
+   /* private JButton membershipButton;
     private JLabel payTypeLabel;
     private JButton payCardButton;
     private JButton giftCardButton;
     private JButton payCashButton;
     private JButton back;
 
+*/
+    private PayWithCard card;
+    private PayWithCash cash;
 
+    public PayAndMembershipGUI(){
+        JFrame frame = new JFrame("Pay & Membership");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
