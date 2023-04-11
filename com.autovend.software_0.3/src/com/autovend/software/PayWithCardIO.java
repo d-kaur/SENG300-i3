fbabe2779833b6147f58dd5e2e7cc2c3255c2f43
deepinderkaur <deepinderkaur1003@gmail.com>
2023-04-11 17:45:01 -0600

    Push??

--- com.autovend.software_0.3/src/com/autovend/software/PayWithCardIO.java
+++ com.autovend.software_0.3/src/com/autovend/software/PayWithCardIO.java
@@ -3,12 +3,11 @@
 package com.autovend.software;
 import javax.swing.*;
 
-import com.autovend.devices.SelfCheckoutStation;
+//import com.autovend.devices.SelfCheckoutStation;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 public class PayWithCardIO extends JFrame
 {
     private PayIO parent;
@@ -16,5 +15,6 @@ public class PayWithCardIO extends JFrame
     public PayWithCardIO(PayIO parent,String msg)
     {
         super(msg);
+        this.parent = parent;
     }
 }
