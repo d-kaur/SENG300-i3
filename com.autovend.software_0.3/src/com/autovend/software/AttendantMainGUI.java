601cddc7422df806f19e261e3b77bf0e2640403a
blahblahblahyo <35211575+blahblahblahyo@users.noreply.github.com>
2023-04-10 18:55:42 -0600

    added preventscreen class
    added preventscreen class

--- com.autovend.software_0.3/src/com/autovend/software/AttendantMainGUI.java
+++ com.autovend.software_0.3/src/com/autovend/software/AttendantMainGUI.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.autovend.software;
 
 import com.autovend.software.*;
@@ -172,3 +173,210 @@ public class AttendantMainGUI extends JFrame implements ActionListener {
     
 }
 
+=======
+package com.autovend.software;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AttendantMainGUI extends JFrame {
+
+    //Setting up button for each self check out station
+    private int stationSelected;
+    private JButton stationButton;
+
+    //Setting up attendant action buttons 
+
+    private JButton startButton;
+    private JButton stopButton;
+    private JButton premitButton;
+    private JButton preventButton;
+    private JButton removeItemButton;
+    private JButton addItemBySearchButton;
+
+    private JButton adjustCoinsBillsButton;
+
+    private JButton allowPersonalBagButton;
+    private JButton approveExceptionButton;
+
+    private JButton addInkPaperButton;
+    public JLabel alert;
+    private boolean[] open = {false,false,false,false,false};
+    private boolean[] permitted = {false,false,false,false,false};
+    private static AttendantIO parent;
+    
+    public AttendantMainGUI(AttendantIO parent) {
+        this.parent = parent;
+        JFrame frame = new JFrame("Attendant Self Checkout Station");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //Setting up Self Checkout Panel Layout
+        JPanel StationPanel = new JPanel();
+        JPanel StartPermitPanel = new JPanel();
+        JPanel StopPreventPanel = new JPanel();
+        JPanel MiddleButtonsPanel = new JPanel();
+        JPanel mainPanel = new JPanel();
+        
+        setMinimumSize(new Dimension(600,400));
+        
+
+        //Setting up contents of Panel
+
+        //Adding station buttons to panel and including action listener to buttons 
+        for (int i = 0; i < 5; i++) {
+            JButton stationButton = new JButton(String.valueOf(i + 1));
+            final int index = i; 
+            stationButton.addActionListener(e -> {
+                stationSelected = index;
+            });
+            StationPanel.add(stationButton);
+        }
+
+        startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        premitButton = new JButton("Permit");
+        premitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        stopButton = new JButton("Stop");
+        stopButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        preventButton = new JButton("Prevent");
+        preventButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        removeItemButton = new JButton("Remove Item");
+        removeItemButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        addItemBySearchButton = new JButton("Add Item By Search");
+        addItemBySearchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        adjustCoinsBillsButton = new JButton("Adjust Coins/Bills");
+        adjustCoinsBillsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        allowPersonalBagButton = new JButton("Allow Personal Bag");
+        allowPersonalBagButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        approveExceptionButton = new JButton("Approve");
+        approveExceptionButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        addInkPaperButton = new JButton("Add Ink/Paper");
+        addInkPaperButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //removeItemFrame removeItem = new removeItemFrame();
+                //removeItemframe.display();
+             }
+            
+        });
+
+        //Adding contents to Panel 
+
+        //Left side buttons
+        StartPermitPanel.setLayout(new BoxLayout(StartPermitPanel, BoxLayout.Y_AXIS));
+        StartPermitPanel.add(startButton);
+        StartPermitPanel.add(premitButton);
+
+        //Right side buttons
+        StopPreventPanel.setLayout(new BoxLayout(StopPreventPanel, BoxLayout.Y_AXIS));
+        StopPreventPanel.add(stopButton);
+        StopPreventPanel.add(preventButton);
+        
+        //Middle Buttons
+        MiddleButtonsPanel.setLayout(new BoxLayout(MiddleButtonsPanel, BoxLayout.Y_AXIS));
+        MiddleButtonsPanel.add(Box.createVerticalGlue());
+        removeItemButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        MiddleButtonsPanel.add(removeItemButton);
+        addItemBySearchButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        MiddleButtonsPanel.add(addItemBySearchButton);
+        adjustCoinsBillsButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        MiddleButtonsPanel.add(adjustCoinsBillsButton);
+        allowPersonalBagButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        MiddleButtonsPanel.add(allowPersonalBagButton);
+        approveExceptionButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        MiddleButtonsPanel.add(approveExceptionButton);
+        addInkPaperButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        MiddleButtonsPanel.add(addInkPaperButton);
+        MiddleButtonsPanel.add(Box.createVerticalGlue());
+
+        //Orienting contents layout
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(StationPanel, BorderLayout.NORTH);
+        mainPanel.add(StartPermitPanel, BorderLayout.WEST);
+        mainPanel.add(StopPreventPanel, BorderLayout.EAST);
+        mainPanel.add(MiddleButtonsPanel, BorderLayout.CENTER);
+        
+        frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setContentPane(mainPanel);
+        pack();
+    }
+    public void showAlert(String message) {
+      JOptionPane.showMessageDialog(this, message, "Alert", JOptionPane.INFORMATION_MESSAGE);
+  }
+    
+    
+
+    public static void main(String[] args){
+      new AttendantMainGUI(parent);
+   }
+
+
+    
+}
+>>>>>>> Stashed changes
--- com.autovend.software_0.3/src/com/autovend/software/CustomerMainGUI.java
+++ com.autovend.software_0.3/src/com/autovend/software/CustomerMainGUI.java
@@ -1,4 +1,166 @@
 package com.autovend.software;
+<<<<<<< Updated upstream
 public class CustomerMainGUI {
     
+=======
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+
+
+
+/**
+ * @author turec
+ *
+ */
+public class CustomerMainGUI extends JFrame implements ActionListener{
+
+
+    private JPanel buttonPanel;
+	private JPanel payPanel;
+	private JPanel displayPanel;
+
+
+
+	public CustomerMainGUI(){
+        buttonPanel = new JPanel();
+        payPanel = new JPanel();
+        displayPanel = new JPanel();
+        setMinimumSize(new Dimension(650, 700)); //set the size of the frame
+
+
+        //set up the buttons
+        JButton scanButton = new JButton("Add by Scanning");//add action listener to this button
+        scanButton.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                //new ScanGUI();
+            }
+        });        
+        JButton pluButton = new JButton("Add by PLU Number");
+        pluButton.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        JButton searchButton = new JButton("Add by Search");
+        searchButton.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        JButton browseButton = new JButton("Add by Browsing Purchase Bags");
+        browseButton.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        JButton purchaseButton = new JButton("Purchase Bags");
+        purchaseButton.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        JButton payButton = new JButton("Pay");
+        payButton.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
+        //payButton.setBounds();
+        AttendantMainGUI alert = new AttendantMainGUI(null);
+        JButton useBag = new JButton("Use Personal Bag");
+        useBag.addActionListener(new ActionListener() {    
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                alert.showAlert("Alert:Use Personal Bag");
+                
+            }
+        });
+
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        payPanel.setLayout(new BoxLayout(payPanel, BoxLayout.X_AXIS));
+        displayPanel.setLayout(new BoxLayout(displayPanel, BoxLayout.Y_AXIS));
+
+        buttonPanel.add(scanButton);
+        buttonPanel.add(pluButton);
+        buttonPanel.add(searchButton);
+        buttonPanel.add(browseButton);
+        buttonPanel.add(purchaseButton);
+        buttonPanel.add(payButton); 
+        buttonPanel.add(useBag);
+
+        payPanel.add(payButton);
+        payPanel.add(useBag);
+
+        add(buttonPanel, BorderLayout.EAST);
+        add(payPanel, BorderLayout.SOUTH);
+
+        //setting up the header label
+        JLabel headerLabel = new JLabel("Items in Cart:");
+        int fontSize = 18;
+        headerLabel.setFont(new Font(headerLabel.getFont().getName(), Font.BOLD, fontSize));
+
+        //setting up the jcombobox to display the items in the cart
+        JComboBox<String> comboBox = new JComboBox<>();
+
+        displayPanel.add(headerLabel);
+        displayPanel.add(comboBox);
+
+        add(displayPanel, BorderLayout.WEST);
+
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);   
+        
+    }
+    
+//	shows that the selfcheckout is not available for use
+    public static void preventScreen(CustomerMainGUI mainGui) {
+        mainGui.buttonPanel.setVisible(false);
+        mainGui.displayPanel.setVisible(false);
+        mainGui.payPanel.setVisible(false);
+        
+        
+        JLabel label = new JLabel("Checkout Disabled", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 54)); // set font size to 36
+        mainGui.add(label);
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+        
+  
+    
+
+    public static void main(String[] args){
+        CustomerMainGUI mainGui = new CustomerMainGUI();
+        
+        preventScreen(mainGui);
+
+//        mainGui.buttonPanel.setVisible(false);
+//        mainGui.displayPanel.setVisible(false);
+//        mainGui.payPanel.setVisible(false);
+        }
+
+   
+>>>>>>> Stashed changes
 }
