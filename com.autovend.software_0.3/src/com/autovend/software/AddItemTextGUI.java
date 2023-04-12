f378a018ab6dd4d2f2d43b2f1fed99e44ac39e81
bheesha.kumari <bheesha.kumari@cs.ucalgary.ca>
2023-04-11 23:11:34 -0600

    Add Item Text GUI

--- com.autovend.software_0.3/src/com/autovend/software/AddItemTextGUI.java
+++ com.autovend.software_0.3/src/com/autovend/software/AddItemTextGUI.java
@@ -6,7 +6,51 @@ package com.autovend.software;
 /**
  * @author wasay
  *
+ * bheesha kumari - 30158810
+ *
  */
 public class AddItemTextGUI {
+
+    public abstract class AddItemTextGUI extends AddItemBrowseGUI {
+        private JButton backButton;
+
+        AddItemTextGUI() {
+            backButton = new JButton();
+
+            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            backButton.setFont(new Font("Segoe UI", 0, 18));
+            backButton.setText("Back");
+            backButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
+                    backButtonActionPerformed(evt);
+                }
+            });
+
+            GroupLayout layout = new GroupLayout(getContentPane());
+            getContentPane().setLayout(layout);
+            layout.setHorizontalGroup(
+                    layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addContainerGap(309, Short.MAX_VALUE)
+                                    .addComponent(backButton, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+                                    .addContainerGap())
+            );
+            layout.setVerticalGroup(
+                    layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addContainerGap(235, Short.MAX_VALUE)
+                                    .addComponent(backButton, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+                                    .addGap(16, 16, 16))
+            );
+
+            pack();
+        }
+
+        protected abstract void backButtonActionPerformed(ActionEvent evt);
+
+
+    }
+
 
 }
