5c1670da13b42ad12779edba3a262e80c8344158
Deepinder Kaur <56122609+deepinder-10@users.noreply.github.com>
2023-04-12 04:08:16 -0600

    added stuff

--- com.autovend.software_0.3/src/com/autovend/software/PayWithGiftCardIO.java
+++ com.autovend.software_0.3/src/com/autovend/software/PayWithGiftCardIO.java
@@ -8,16 +8,58 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class PayWithGiftCardIO extends  PayGUI
-{
+public class PayWithGiftCardIO extends PayGUI {
+	//GIFT IS ONLY BY INSERT
+	//DO we even need a button?
+	private JButton insertButton;
+	private JButton backButton;
+
 	private JButton back = new JButton("back");
 	 public PayWithGiftCardIO(String msg, SelfCheckoutStation station, PurchasedItems list, PayIO parent) {
 			super(msg,station,list,parent);
-			back.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
 
-				}
-			});
-		}
+		 JFrame frame = new JFrame("Pay with Gift Card");
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		 JPanel actionButtonsPanel = new JPanel();
+		 JPanel backPanel = new JPanel();
+		 JPanel mainPanel = new JPanel();
+
+		 setMinimumSize(new Dimension(500,300));
+
+		 JLabel headerLabel = new JLabel("Please Insert Card");
+		 insertButton = new JButton("INSERT");
+		 insertButton.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e){
+
+			 }
+		 });
+
+		 actionButtonsPanel.setLayout(new BoxLayout(actionButtonsPanel, BoxLayout.Y_AXIS));
+		 actionButtonsPanel.add(Box.createVerticalGlue());
+		 headerLabel.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		 actionButtonsPanel.add(headerLabel);
+		 insertButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		 actionButtonsPanel.add(insertButton);
+
+		 backButton = new JButton("Back");
+		 backButton.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e){
+
+			 }
+		 });
+
+		 backPanel.add(backButton);
+		 mainPanel.setLayout(new BorderLayout());
+		 mainPanel.add(actionButtonsPanel, BorderLayout.NORTH);
+		 mainPanel.add(backPanel, BorderLayout.SOUTH);
+
+		 frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+
+		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 setVisible(true);
+		 setContentPane(mainPanel);
+		 pack();
+
+	 }
 }
