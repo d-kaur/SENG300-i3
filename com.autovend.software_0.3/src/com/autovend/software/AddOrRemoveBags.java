71d97f17e56fd6baf9851bff03e9c66429812c26
James Wang <127716520+fuNNy325@users.noreply.github.com>
2023-04-12 03:10:50 -0600

    Add files for bagging manually

--- com.autovend.software_0.3/src/com/autovend/software/AddOrRemoveBags.java
+++ com.autovend.software_0.3/src/com/autovend/software/AddOrRemoveBags.java
@@ -14,51 +14,55 @@
  */
 package com.autovend.software;
 
-import java.math.*;
-import java.util.ArrayList;
-
 import com.autovend.devices.*;
+import com.autovend.devices.observers.AbstractDeviceObserver;
+import com.autovend.devices.observers.ReusableBagDispenserObserver;
+
+import static com.autovend.software.PurchasedItems.addBag;
 
 // extend the Parent class
-public class AddOrRemoveBags{
-    /*ArrayList<Bag> listOfStoreBags;
+public class AddOrRemoveBags implements ReusableBagDispenserObserver {
+
+    private SelfCheckoutStation station;
 
     public AddOrRemoveBags(SelfCheckoutStation scs) {
-        super(scs);
-
-        listOfStoreBags = new ArrayList<Bag>();
-
-        Bag reusableBagSmall = new Bag("Small Reusable Bag", new BigDecimal("0.25"), 0.1);
-        listOfStoreBags.add(reusableBagSmall);
-        Bag reusableBagMedium = new Bag("Medium Reusable Bag", new BigDecimal("0.50"), 0.2);
-        listOfStoreBags.add(reusableBagMedium);
-        Bag reusableBagLarge = new Bag("Large Reusable Bag", new BigDecimal("1"), 0.3);
-        listOfStoreBags.add(reusableBagLarge);
-        Bag disposableBagSmall = new Bag("Small Disposable Bag", new BigDecimal("0.25"), 0.1);
-        listOfStoreBags.add(disposableBagSmall);
-        Bag disposableBagMedium = new Bag("Medium Disposable Bag", new BigDecimal("0.50"), 0.2);
-        listOfStoreBags.add(disposableBagMedium);
-        Bag disposableBagLarge = new Bag("Large Disposable Bag", new BigDecimal("1"), 0.3);
-        listOfStoreBags.add(disposableBagLarge);
+        this.station = scs;
     }
 
-    public void purchaseBag(Bag typeofBag) throws SimulationException {
-
-        if (typeofBag == null ) {
-            throw new SimulationException("Please select a valid bag type.");
+    public void purchaseBag(int BagNum) throws EmptyException {
+        for (int i = 0; i < BagNum; i++) {
+            addBag(this.station.bagDispenser.dispense());
         }
-        for(Bag bag : listOfStoreBags) {
-            if(bag.getWeight() == typeofBag.getWeight() && bag.getPrice().equals(typeofBag.getPrice()) && bag.getDescription().equals(typeofBag.getDescription())) {
-                addBag(bag);
-                return;
-            }
-        }
-        throw new SimulationException("Please select a valid bag type.");
     }
 
-    public void addOwnBag(double bagWeight) {
-        Bag ownBag = new Bag("Own Bag", new BigDecimal(0.000001), bagWeight);
-        addBag(ownBag);
-        itemsBought.addAmountPaid(ownBag.getPrice()); // This is done to avoid the exception thrown by the addBag method as the price cannot be zero
-    }*/
+    public static boolean addOwnBag(double bagWeight) {
+    	if (bagWeight <= 0) {
+    		return false;
+    	}
+		return true;
+
+    }
+    @Override
+    public void bagDispensed(ReusableBagDispenser dispenser) {
+    }
+
+    @Override
+    public void outOfBags(ReusableBagDispenser dispenser) {
+        System.out.println("Sorry, the bags at the current site are sold out");
+    }
+
+    @Override
+    public void bagsLoaded(ReusableBagDispenser dispenser, int count) {
+
+    }
+
+    @Override
+    public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+        this.station.bagDispenser.enable();
+    }
+
+    @Override
+    public void reactToDisabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+        this.station.bagDispenser.disable();
+    }
 }
